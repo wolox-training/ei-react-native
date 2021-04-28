c73b6cb1637be0d4a9f2099addb05dc5b15a5079
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-04-28 15:56:23 -0300

    ReactoTronConfig init

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -1,7 +1,12 @@
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+}
+import Reactotron from 'reactotron-react-native';
 import React from 'react';
 
 import App from './src/app';
 
 export default function index() {
+  Reactotron.log({ numbers: [1, 2, 3], boolean: false, nested: { here: 'we go' } });
   return <App />;
 }

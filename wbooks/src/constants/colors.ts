3fd0d0d34525ddd35898d163ea2a8f51fe119f43
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-07 13:34:28 -0300

    change colors name

--- wbooks/src/constants/colors.ts
+++ wbooks/src/constants/colors.ts
@@ -1,7 +1,7 @@
 export const blue = '#6495ED';
 export const white = '#FFF';
 export const black = '#323232';
-export const gray = '#e1e1e1';
+export const gray = '#E1E1E1';
 export const whiteBlue = '#E5EDF7';
 export const cyan = '#1EC9E5';
 export const lightGray = '#C4D8EB';

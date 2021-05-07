d0f1d53f593f45d294982edc3155ea1c7d0dea98
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-07 20:30:31 -0300

    fix reactotrontest

--- wbooks/ReactotronConfig.js
+++ wbooks/ReactotronConfig.js
@@ -2,6 +2,9 @@ import { reactotronRedux } from 'reactotron-redux';
 import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const reactotron = Reactotron.configure({ name: 'React Native Demo' }).use(reactotronRedux()).connect();
-Reactotron.setAsyncStorageHandler(AsyncStorage).configure().useReactNative().connect();
+const reactotron = Reactotron.configure({ name: 'React Native Demo' })
+  .setAsyncStorageHandler(AsyncStorage)
+  .use(reactotronRedux())
+  .useReactNative({})
+  .connect();
 export default reactotron;

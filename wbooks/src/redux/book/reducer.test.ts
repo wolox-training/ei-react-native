f1540f4f267d67a47eb55be90f3656a6a5a8fc4e
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-13 18:40:57 -0300

    added reducer and actions tests, added redux-mock-store

---
+++ wbooks/src/redux/book/reducer.test.ts
@@ -0,0 +1,45 @@
+import { BOOKS_MOCK } from '@constants/mockBooks';
+import { BookInterface } from '@interfaces/book';
+
+import BookListReducer, { initialState } from './reducer';
+import { actions, TARGETS } from './actions';
+
+const MOCKED_TEST = BOOKS_MOCK[0];
+const books: BookInterface[] = [MOCKED_TEST];
+
+export const booksActions = {
+  getListBook: {
+    type: actions.GET_BOOKS,
+    target: TARGETS.BOOKS
+  },
+  getListBookSuccess: {
+    type: actions.GET_BOOKS_SUCCESS,
+    target: TARGETS.BOOKS,
+    payload: books
+  },
+  getListBookFailure: {
+    type: actions.GET_BOOKS_FAILURE,
+    target: TARGETS.BOOKS,
+    payload: true
+  }
+};
+describe('BOOKSLIST', () => {
+  test('GET_BOOKLIST', () => {
+    expect(BookListReducer(undefined, booksActions.getListBook)).toMatchObject({
+      ...initialState,
+      booksLoading: true
+    });
+  });
+  test('GET_BOOKLIST_SUCCESS', () => {
+    expect(BookListReducer(undefined, booksActions.getListBookSuccess)).toMatchObject({
+      ...initialState,
+      books
+    });
+  });
+  test('GET_BOOKLIST_FAILURE', () => {
+    expect(BookListReducer(undefined, booksActions.getListBookFailure)).toMatchObject({
+      ...initialState,
+      booksError: true
+    });
+  });
+});

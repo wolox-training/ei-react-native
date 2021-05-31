475644c386c4e8f8cbf6385ea0dcacc3a37d3793
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-05-31 13:54:45 -0300

    fixed constants detailBook

---
+++ wbooks/src/app/screens/DetailBook/constants.ts
@@ -0,0 +1,9 @@
+export const ADD_WISH = 'Add to whislist';
+export const CHECK = '✔';
+export const EMPTY = '';
+export const ANIMATE_DURATION = 2000;
+export const USE_NATIVE_DRIVE = false;
+export enum IMAGE {
+  RESIZE_METHOD = 'auto',
+  RESIZE_MODE = 'contain'
+}
